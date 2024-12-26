7bd4da7ee420f943d0db8ecc587d9091176a48f5
Abai369 <jcaleb1110@gmail.com>
2024-12-26 15:16:07 -0800

    feat navbar

---
+++ artist_gallery/src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './NavBar.css';
+
+function Navbar() {
+    return (
+        <nav className="navbar">
+            <ul className="navbar__list">
+                <li classname="navbar__item"><a href="about-me">About Me</a></li>
+                <li className="navbar__item"><a href="#gallery">Gallery</a></li>
+                <li className = "navbar__item"><a href="#buy-form">Buy</a></li>
+            </ul>
+        </nav>
+    );
+}
+
+export default Navbar;

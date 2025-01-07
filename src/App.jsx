c8ad927c02979722c42c1c4409bf7103aa9ce0ad
Abai369 <jcaleb1110@gmail.com>
2025-01-06 19:47:19 -0800

    feat App.css

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import Buy from './pages/Buy';
 import AboutMe from './pages/Aboutme';
 import Gallery from './pages/Gallery';
+import './styles/App.css';
 
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
             <Routes>
             <Route path="/" element={<Gallery/>} />
             <Route path="/buy" element={<Buy />} />
+            <Route path="/gallery" element={<Gallery />} />
             <Route path="/about-me" element={<AboutMe />} />
             </Routes>
         </Router>

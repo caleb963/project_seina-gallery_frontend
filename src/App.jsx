d93203d43269bb7528da2fae366dfba4e87b8145
Abai369 <jcaleb1110@gmail.com>
2025-01-08 11:06:22 -0800

    feat corrections

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
-import Navbar from './pages/Navbar';
+import Navbar from './components/Navbar';
 import Home from './pages/Home';
 import AboutMe from './pages/Aboutme';
 import Gallery from './pages/Gallery';
@@ -12,6 +12,7 @@ function App() {
     return (
     <PaypalScriptProvider options={{ "client-id": "AZPlhFVjoili6Jzeu704M_gDNmju2Ufs9OeDbAC6ivvtgWmtHIAGbqRReQbsDM3Ujfw_wtiSaz1klUqf" }}>
         <Router>
+            <Navbar />
             <Routes>
             <Route path="/" element={<Home/>} />
             <Route path="/buy" element={<AboutMe />} />

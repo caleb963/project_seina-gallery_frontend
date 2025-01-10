5ed5a9df8b0eaaebe744c445b764f495ecc75253
Abai369 <jcaleb1110@gmail.com>
2025-01-10 00:21:54 -0800

    fix styles dor aboutme

--- src/pages/Aboutme.jsx
+++ src/pages/Aboutme.jsx
@@ -2,13 +2,13 @@ import React from 'react';
 import '../styles/Aboutme.css'
 
 function Aboutme() {
-    <section id="about-me">
-            <h1>About me</h1>
-            <h2>Seina: Art That Bridges Worlds</h2>
-            <p>Seina Solo is a multidisciplinary artist whose work embodies a journey through the mind, soul, and history. With a background in anthropology and psychology, Seina blends academic insight and personal experiences to create pieces that transcend the visual, inviting viewers into a profound reflection on humanity's connection with the universe.</p>
-            <p>Her art delves into altered states of mind, inspired by her explorations with psychedelic plants and the spirituality of ancient cultures. Each stroke tells a story, inviting viewers to step into a realm where the tangible and the ethereal intertwine.</p>
-            <p>In addition to her fascination with Anthropology,Seina draws inspiration from modern technology, music, and video games. These elements infuse her work with a dynamic energy, bridging ancient traditions with contemporary culture. The rhythmic interplay of sound, the immersive worlds of gaming, and the endless possibilities of digital tools influence her creative process, resulting in art that feels both timeless and cutting-edge.</p>
-            <p>Seina's vision is to awaken in her audience a sense of universal belonging, celebrating both the diversity and commonality of human experiences across time and space. Her art is not merely observed—it is felt—becoming a bridge to the ancestral, the introspective, and the innovative.</p>.
+    <section className="about-me">
+            <h1 ClassName="about-me__subtitle">About me</h1>
+            <h2 className="about-me__subtitle">Seina: Art That Bridges Worlds</h2>
+            <p className="about-me__paragraph">Seina Solo is a multidisciplinary artist whose work embodies a journey through the mind, soul, and history. With a background in anthropology and psychology, Seina blends academic insight and personal experiences to create pieces that transcend the visual, inviting viewers into a profound reflection on humanity's connection with the universe.</p>
+            <p className="about-me__paragraph">Her art delves into altered states of mind, inspired by her explorations with psychedelic plants and the spirituality of ancient cultures. Each stroke tells a story, inviting viewers to step into a realm where the tangible and the ethereal intertwine.</p>
+            <p className="about-me__paragraph">In addition to her fascination with Anthropology,Seina draws inspiration from modern technology, music, and video games. These elements infuse her work with a dynamic energy, bridging ancient traditions with contemporary culture. The rhythmic interplay of sound, the immersive worlds of gaming, and the endless possibilities of digital tools influence her creative process, resulting in art that feels both timeless and cutting-edge.</p>
+            <p className="about-me__paragraph about-me__paragraph--highlightened">Seina's vision is to awaken in her audience a sense of universal belonging, celebrating both the diversity and commonality of human experiences across time and space. Her art is not merely observed—it is felt—becoming a bridge to the ancestral, the introspective, and the innovative.</p>.
         </section>
   }
 
